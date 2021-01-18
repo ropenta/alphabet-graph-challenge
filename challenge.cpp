67d4444970b94b597775235d97e34db44783135d
Rohith Pentaparthy <penta@umich.edu>
2021-01-17 22:18:29 -0500

    invalid small inputs

--- challenge.cpp
+++ challenge.cpp
@@ -6,7 +6,7 @@
 #include <vector>
 #include "catch.hpp" // unit testing with Catch2
 
-using namespace std; // possibly remove this
+using namespace std;
 
 struct Node {
     char c;                         // current char
@@ -18,23 +18,45 @@ struct Node {
     }
 };
 
-/* Solve for alphabet   */
-/* Input: list of words */
-/* Output: alphabet     */
-string findAlphabet(vector<string> &words) {
-    /* TODO */
-    if (words.size() < 1) {
-        return "";
-    }
-
-    // 0. create data structures
-    unordered_map<char, Node*> nodes;
-    unordered_set<char> zeroInCount;
-    stack<Node*> nextLetters;
+/* Input: stack of next letters to go into alphabet*/
+/* Modifies: stack */
+/* Returns: alphabet in string format */
+string createAlphabet(stack<Node*> &nextLetters) {
     string alphabet = "";
+    while (!nextLetters.empty()) {
+        Node *letter = nextLetters.top();
+        nextLetters.pop();
+        alphabet += letter->c;
 
-    // 1. compare adjacent words, find first difference if it exists
-    /* TODO add this in helper function */
+        for (auto &n: letter->nextNeighbors) {
+            n->inCount--;
+            if (n->inCount < 1) {
+                // multiple letters pushed in a loop can lead to multiple possible alphabets
+                nextLetters.push(n);
+            }
+        }
+    }
+    return alphabet;
+}
+
+/* Input: graph, set of letters with no inbound nodes pointing to them, empty stack of next letters to go into alphabet*/
+/* Modifies: graph, set of letters, stack of next alphabet letters */
+void addFirstLetter(unordered_map<char, Node*> &nodes, unordered_set<char> &zeroInCount, stack<Node*> &nextLetters) {
+    if (zeroInCount.size() < 1) {
+        cout << "No alphabet found\n";
+        return;
+    }
+    char c = *zeroInCount.begin();
+    Node *n1 = nodes[c];
+    nextLetters.push(n1);
+}
+
+/* Input: word list, empty graph, set of letters with no inbound nodes pointing to them */
+/* Modifies: graph, set of letters */
+void createDirectedGraph(
+        vector<string> &words, 
+        unordered_map<char, Node*> &nodes, 
+        unordered_set<char> &zeroInCount) {
     for (int i = 0; i < words.size() - 1; i++) {
         string firstWord = words[i];
         string secondWord = words[i + 1];
@@ -70,36 +92,69 @@ string findAlphabet(vector<string> &words) {
             }
         }
     }
+}
 
-    // 2. find first letter
-    /* TODO: handle errors in a more standard way (try-catch, exception) */
-    if (zeroInCount.size() < 1) {
-        exit(1);
-    }
-    char c = *zeroInCount.begin();
-    Node *n1 = nodes[c];
-    nextLetters.push(n1);
-
-    // 3. add curr item to alphabet, get neighbors, decrease inCount, add zero in count nodes to stack
-    while (!nextLetters.empty()) {
-        Node *letter = nextLetters.top();
-        nextLetters.pop();
-        alphabet += letter->c;
-
-        for (auto &n: letter->nextNeighbors) {
-            n->inCount--;
-            if (n->inCount < 1) {
-                nextLetters.push(n);
-            }
+/* Input: list of words */
+/* Output: alphabet     */
+string findAlphabet(vector<string> &words) {
+    /* TODO */
+    if (words.size() < 1) {
+        return "";
+    } else if (words.size() == 1) {
+        // edge case: 1 word with 1 letter
+        if (words[0].size() == 1) {
+            return words[0];
+        } else {
+            return "";
         }
     }
 
+    // 0. create data structures
+    unordered_map<char, Node*> nodes;
+    unordered_set<char> zeroInCount;
+    stack<Node*> nextLetters;
+    string alphabet = "";
+
+    // 1. compare adjacent words, find first difference if it exists
+    createDirectedGraph(words, nodes, zeroInCount);
+
+    // 2. find and add first letter, if it is unambiguous
+    /* TODO: handle errors in a more standard way (try-catch, exception) */
+    addFirstLetter(nodes, zeroInCount, nextLetters);
+
+    // 3. add curr item to alphabet, get neighbors, decrease inCount, add zero in count nodes to stack
+    alphabet = createAlphabet(nextLetters);
     return alphabet;
 }
 
-TEST_CASE( "Get alphabet") {
+TEST_CASE( "Valid alphabets") {
     vector<string> sampleTest = {"bca", "aaa", "acb"};
     REQUIRE( findAlphabet(sampleTest) == "bac" );
     vector<string> firstAlphabetLetterNestedInWord = {"bb", "bh", "hb", "hc", "hd", "dc", "dh", "ddg", "ddb"};
     REQUIRE( findAlphabet(firstAlphabetLetterNestedInWord) == "gbchd" );
 }
+
+TEST_CASE( "Valid small inputs") {
+    vector<string> noWords = {};
+    vector<string> oneLetter = {"h"};
+    vector<string> oneWord = {"hi"};
+    REQUIRE( findAlphabet(noWords) == "");
+    REQUIRE( findAlphabet(oneLetter) == "h");
+    REQUIRE( findAlphabet(oneWord) == "");
+}
+
+TEST_CASE( "Invalid small inputs") {
+    vector<string> oneWord = {"hi"};
+    REQUIRE( findAlphabet(oneWord) == "");
+}
+
+// TEST_CASE( "Duplicate words") {
+//     vector<string> sameWords = {"hi"};
+//     vector<string> duplicatesAtEnd = {"bb", "bh", "hb", "hb"};
+//     vector<string> duplicatesAtStart = {"bb", "bb", "hb", "hi"};
+//     vector<string> multipleDuplicates = {"bb", "bb", "hb", "hb"};
+//     REQUIRE( findAlphabet(sameWords) == "hi");
+//     REQUIRE( findAlphabet(duplicatesAtEnd) == "bh");
+//     REQUIRE( findAlphabet(duplicatesAtStart) == "bhi"); // ambiguous
+//     REQUIRE( findAlphabet(multipleDuplicates) == "bh");
+// }
